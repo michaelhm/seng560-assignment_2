6422293cab4048c8b0db924d79403a4e331b166a
michaelhm <mclinden.michael@gmail.com>
2021-11-19 12:54:21 -0500

    rename src to lib Move readme into lib

---
+++ lib/zroberts-seng560-math/math560.py
@@ -0,0 +1,112 @@
+# TODO - raise an exception in add/subtract/multiply/divide if a/b are not ints, same in list additions,multiplcations, etc... 
+
+def addition(a, b):
+    return a + b
+    
+def sumList(items):
+    if(type(items) is not list):
+        raise Exception("List not provided")
+    total = 0
+    for i in items:
+        total += i
+    return total
+
+def subtract(a,b):
+    return a - b
+
+def subtractList(items):
+    if(type(items) is not list):
+        raise Exception("List not provided")
+    total = items[0]
+    for i in items[1:]:
+        total -= i
+    return total
+
+def multiply(a, b):
+    return a * b
+
+def multiplyList(items):
+    if(type(items) is not list):
+        raise Exception("List not provided")
+    total = items[0]
+    for i in items[1:]:
+        total *= i
+    return total
+
+def divide(a, b):
+    return a / b
+
+def squareRoot(x):
+    return x ** .5
+
+def exponent(base,expon):
+    total = base; 
+    i = 1
+    while i < expon:
+        total *= base
+        i += 1
+    return total
+
+
+def convertToBinary(x):
+    if type(x) is not int:
+        raise Exception("Int not provided")
+
+    binNum = [0] * x   
+    i = 0
+    while(x > 0):
+        binNum[i] = x % 2
+        x = int(x / 2)
+        i += 1
+    returnString = ""
+    for j in range (i - 1, -1, -1):
+        returnString += str(binNum[j])
+    return returnString
+
+def convertToHex(x):
+    if type(x) is not int:
+        raise Exception("Int not provided")
+    
+    hexaNum = ['0'] * 100
+    i = 0
+    while (x != 0):
+        temp = 0
+        temp = x % 16
+        if(temp < 10):
+            hexaNum[i] = chr(temp + 48)
+        else:
+            hexaNum[i] = chr(temp + 55)
+        i = i + 1
+        x = int(x /16)
+
+    j = i - 1
+    returnString = ""
+    while (j >= 0):
+        returnString += str(hexaNum[j])
+        j = j - 1
+    
+    return returnString
+
+def convertToOct(x):
+    if type(x) is not int:
+        raise Exception("Int not provided")
+    
+    octalNum = [0] * 100
+
+    i = 0
+    while x != 0:
+        octalNum[i] = x % 8
+        x = int (x / 8)
+        i += 1
+    
+    returnString = ""
+    for j in range (i -1, -1, -1):
+        returnString += str(octalNum[j])
+
+    return returnString
+
+
+def convertToInt():
+    # TODO - convert value to INT
+    return "" 
+
