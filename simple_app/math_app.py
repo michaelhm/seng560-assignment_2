6b6a61ba4c5cf6fe2b4c9a84332ca18de0ecace8
michaelhm <mclinden.michael@gmail.com>
2021-11-19 13:09:09 -0500

    Update lib structure Import math560 into math_app

---
+++ simple_app/math_app.py
@@ -0,0 +1,6 @@
+# A simple math application built by reusing a Pytnon library
+# Library: https://github.com/zroberts/seng560-python-math
+# Forked repository to build app and reuse library
+
+from lib import math560
+
